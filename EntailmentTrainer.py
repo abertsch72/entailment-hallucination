520dfb655686d394224a80132d6ac369cd237f57
Ubuntu <ubuntu@ip-172-31-14-161.us-east-2.compute.internal>
2021-11-28 17:33:01 +0000

    Minor modifications for making it run on AWS

--- EntailmentTrainer.py
+++ EntailmentTrainer.py
@@ -17,9 +17,10 @@ class EntailmentTrainer(Seq2SeqTrainer):
         # non-differentiable because of model generation? how to handle this?
         # might just work naively?
         loss, outputs = super().compute_loss(model, inputs, return_outputs=True)
-        gen_outputs = model.generate(inputs.input_ids, attention_mask=inputs.attention_mask)
+        #print(inputs.keys())
+        gen_outputs = model.generate(inputs["input_ids"], attention_mask=inputs["attention_mask"])
         gen_outputs = self.tokenizer.batch_decode(gen_outputs, skip_special_tokens=True)
-        input_docs = self.tokenizer.batch_decode(inputs.input_ids, skip_special_tokens=True)
+        input_docs = self.tokenizer.batch_decode(inputs["input_ids"], skip_special_tokens=True)
 
         #print("contradiction score is", self.inference_score([["I am doing well", "I am sick"]]))
         entail_pairs = []
@@ -28,8 +29,8 @@ class EntailmentTrainer(Seq2SeqTrainer):
 
         custom_loss = self.inference_score(entail_pairs)
         custom_loss = torch.mean(custom_loss)
-
-        custom_loss += loss
+        custom_loss = custom_loss.to('cuda')
+        custom_loss += loss.to('cuda')
 
         return (custom_loss, outputs) if return_outputs else custom_loss
 
